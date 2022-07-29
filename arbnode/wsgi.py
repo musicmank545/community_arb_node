23cd0d5664aabcfa4795cd01ab10e35f30d3e0f0
musicmank545 <55155882+musicmank545@users.noreply.github.com>
2022-07-29 11:52:04 -0400

    Update wsgi.py

--- arbnode/wsgi.py
+++ arbnode/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.utils.regex_helper import _lazy_re_compile
+import django.http.request
+
+django.http.request.host_validation_re = _lazy_re_compile(r"[a-zA-z0-9.:]*")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arbnode.settings')
 #os.environ.setdefault('PROMETHEUS_MULTIPROC_DIR', '/home/ghost/prom_tmp/')
